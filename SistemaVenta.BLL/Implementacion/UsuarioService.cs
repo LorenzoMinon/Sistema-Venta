dbbd27b38352d7f2d1f635614b7c829bbf195bf4
Lorenzo Miñón <lorenzominon01@gmail.com>
2023-07-25 18:23:57 -0300

    Se agregan servicios de usuario.

---
+++ SistemaVenta.BLL/Implementacion/UsuarioService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.BLL.Implementacion
+{
+    internal class UsuarioService
+    {
+    }
+}
---
+++ SistemaVenta.BLL/Interfaces/IUsuarioService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.BLL.Interfaces
+{
+    internal interface IUsuarioService
+    {
+    }
+}
