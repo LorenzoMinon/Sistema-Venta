e6fa9832937ea79076aa38adf4bce3c40e777cea
Lorenzo Miñón <lorenzominon01@gmail.com>
2023-09-26 09:28:30 -0300

    Mejoras de código: se borran saltos de línea y código no funcional

--- SistemaVenta.AplicacionWeb/wwwroot/js/vistas/Usuario_Index.js
+++ SistemaVenta.AplicacionWeb/wwwroot/js/vistas/Usuario_Index.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
             { "data": "idUsuario", "visible": false, "searchable": false },
             {
                 "data": "urlFoto", render: function (data) {
-                    return `<img style="height:60px" src=${data} class="roundex mx-auto d-block"/>`
+                    return `<img style="height:60px" src=${data} class="rounded mx-auto d-block"/>`
                 }
             },
             { "data": "nombre" },
@@ -53,7 +53,7 @@ $(document).ready(function () {
                         return '<span class="badge badge-info">Activo</span>';
                     else
                         return '<span class ="badge badge-danger">No Activo</span>';
-                },
+                }
             },
             {
                 "defaultContent": '<button class="btn btn-primary btn-editar btn-sm mr-2"><i class="fas fa-pencil-alt"></i></button>' +
@@ -90,11 +90,100 @@ function mostrarModal(modelo = MODELO_BASE) { //con jquery
     $("#cboRol").val(modelo.idRol == 0 ? $("#cboRol option:first").val() : modelo.idRol)
     $("#cboEstado").val(modelo.esActivo)
     $("#txtFoto").val("")
-    $("#imgUsuario").attr("src", modelo.urlFoto)
+    $("#imgUsuario").attr("src",modelo.urlFoto)
 
     $("#modalData").modal("show")
 }
 
 $("#btnNuevo").click(function () {
     mostrarModal()
+})
+
+$("#btnGuardar").click(function () {
+
+    //debugger;
+
+    const inputs = $("input.input-validar").serializeArray();
+    const inputs_sin_valor = inputs.filter((item) => item.value.trim() == "")
+    // con esto validamos que inputs tienen valor o no. 
+
+    if (inputs_sin_valor.length > 0) {
+        const mensaje = `Debe completar el campo: "${inputs_sin_valor[0].name}"`;
+        toastr.warning("", mensaje) // sale del wwwroot, vendor y el toastr.
+        $(`input[name="${inputs_sin_valor[0].name}"]`).focus()
+        return;
+    }
+
+    const modelo = structuredClone(MODELO_BASE);
+    modelo["idUsuario"] = parseInt($("#txtId").val())
+    modelo["nombre"] = $("#txtNombre").val()
+    modelo["correo"] = $("#txtCorreo").val()
+    modelo["telefono"] = $("#txtTelefono").val()
+    modelo["idRol"] = $("#cboRol").val()
+    modelo["esActivo"] = $("#cboEstado").val()
+
+    const inputFoto = document.getElementById("txtFoto")
+
+    const formData = new FormData();
+
+    formData.append("foto", inputFoto.files[0]) //Definidos para la controladora de usuarios en la funcion Crear.
+    formData.append("modelo", JSON.stringify(modelo))
+
+    $("#modalData").find("div.modal-content").LoadingOverlay("show");
+
+    if (modelo.idUsuario == 0) { // 0 para crear.
+
+        fetch("/Usuario/Crear", {
+            method: "POST",
+            body: formData
+        })
+            .then(response => {
+                $("#modalData").find("div.modal-content").LoadingOverlay("hide");
+                return response.ok ? response.json() : Promise.reject(response);
+            })
+            .then(responseJson => {
+                if (responseJson.estado) {
+                    tablaData.row.add(responseJson.objeto).draw(false)
+                    $("#modalData").modal("hide")
+                    swal("Listo!", "Usuario creado", "success")
+                } else {
+                    swal("Lo sentimos", responseJson.mensaje, "error")
+                }
+            })
+    } else {
+        fetch("/Usuario/Editar", {
+            method: "PUT",
+            body: formData
+        })
+            .then(response => {
+                $("#modalData").find("div.modal-content").LoadingOverlay("hide");
+                return response.ok ? response.json() : Promise.reject(response);
+            })
+            .then(responseJson => {
+                if (responseJson.estado) {
+                    tablaData.row(filaSeleccionada).data(responseJson.objeto).draw(false);
+                    filaSeleccionada = null;
+                    $("#modalData").modal("hide")
+                    swal("Listo!", "Usuario modificado", "success")
+                } else {
+                    swal("Lo sentimos 1", responseJson.mensaje, "error")
+                }
+            })
+    }
+    
+
+})
+
+let filaSeleccionada;
+$("#tbdata tbody").on("click", ".btn-editar", function () {
+
+    if ($(this).closest("tr").hasClass("child")) {
+        filaSeleccionada = $(this).closest("tr").prev();
+    } else {
+        filaSeleccionada = $(this).closest("tr");
+    }
+    const data = tablaData.row(filaSeleccionada).data();
+
+    mostrarModal(data);
+
 })
