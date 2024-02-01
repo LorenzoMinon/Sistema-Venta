02e24b80f441a76c2475e61f88fa4639a3bc4e7b
Lorenzo Miñón <lorenzominon01@gmail.com>
2024-02-01 12:06:15 -0300

    Implementación del modulo categoria

--- SistemaVenta.AplicacionWeb/wwwroot/js/vistas/Categoria_index.js
+++ SistemaVenta.AplicacionWeb/wwwroot/js/vistas/Categoria_index.js
@@ -1,1 +1,197 @@
-﻿
+﻿const MODELO_BASE = {
+    idCategoria: 0,
+    descripcion: "",
+    esActivo: 1,
+}
+
+let tablaData;
+
+
+//jquery
+$(document).ready(function () {
+
+
+    tablaData = $('#tbdata').DataTable({
+        responsive: true,
+        "ajax": {
+            "url": '/Categoria/Lista',
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "idCategoria", "visible": false, "searchable": false },
+            { "data": "descripcion" },
+            {
+                "data": "esActivo", render: function (data) {
+                    if (data == 1)
+                        return '<span class="badge badge-info">Activo</span>';
+                    else
+                        return '<span class ="badge badge-danger">No Activo</span>';
+                }
+            },
+            {
+                "defaultContent": '<button class="btn btn-primary btn-editar btn-sm mr-2"><i class="fas fa-pencil-alt"></i></button>' +
+                    '<button class="btn btn-danger btn-eliminar btn-sm"><i class="fas fa-trash-alt"></i></button>',
+                "orderable": false,
+                "searchable": false,
+                "width": "80px"
+            }
+        ],
+        order: [[0, "desc"]], //Desde idCategoria
+        dom: "Bfrtip",
+        buttons: [
+            {
+                text: 'Exportar Excel',
+                extend: 'excelHtml5',
+                title: '',
+                filename: 'Reporte Categorias ',
+                exportOptions: {
+                    columns: [1,2] //el id es el 0.
+                }
+            }, 'pageLength'
+        ],
+        language: { //para cambiar el idioma del datatable
+            url: "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json"
+        },
+    });
+})
+
+function mostrarModal(modelo = MODELO_BASE) { //con jquery
+    $("#txtId").val(modelo.idCategoria)
+    $("#txtDescripcion").val(modelo.descripcion)
+    $("#cboEstado").val(modelo.esActivo)
+
+    $("#modalData").modal("show")
+}
+
+$("#btnNuevo").click(function () {
+    mostrarModal()
+})
+
+$("#btnGuardar").click(function () {
+
+    if ($("#txtDescripcion").val().trim() == ""){
+        toast.warning("", "Debe completar el campo : descripcion")
+        $("#txtDescripcion").focus() //el cursor se mueve directamente al campo con txtdesc
+        return;
+    }
+
+
+    const modelo = structuredClone(MODELO_BASE);
+    modelo["idCategoria"] = parseInt($("#txtId").val())
+    modelo["descripcion"] = $("#txtDescripcion").val()
+    modelo["esActivo"] = $("#cboEstado").val()
+
+    $("#modalData").find("div.modal-content").LoadingOverlay("show");
+
+    if (modelo.idCategoria == 0) { // 0 para crear.
+
+        fetch("/Categoria/Crear", {
+            method: "POST",
+            headers: { "Content-Type": "application/json; charset=utf-8" },
+            body: JSON.stringify(modelo)
+        })
+            .then(response => {
+                $("#modalData").find("div.modal-content").LoadingOverlay("hide");
+                return response.ok ? response.json() : Promise.reject(response);
+            })
+            .then(responseJson => {
+                if (responseJson.estado) {
+                    tablaData.row.add(responseJson.objeto).draw(false)
+                    $("#modalData").modal("hide")
+                    swal("Listo!", "Categoria creada", "success")
+                } else {
+                    swal("Lo sentimos", responseJson.mensaje, "error")
+                }
+            })
+    } else {
+        fetch("/Categoria/Editar", {
+            method: "PUT",
+            headers: { "Content-Type": "application/json; charset=utf-8" },
+            body: JSON.stringify(modelo)
+        })
+            .then(response => {
+                $("#modalData").find("div.modal-content").LoadingOverlay("hide");
+                return response.ok ? response.json() : Promise.reject(response);
+            })
+            .then(responseJson => {
+                if (responseJson.estado) {
+                    tablaData.row(filaSeleccionada).data(responseJson.objeto).draw(false);
+                    filaSeleccionada = null;
+                    $("#modalData").modal("hide")
+                    swal("Listo!", "Categoria modificada", "success")
+                } else {
+                    swal("Lo sentimos 1", responseJson.mensaje, "error")
+                }
+            })
+    }
+
+})
+let filaSeleccionada;
+$("#tbdata tbody").on("click", ".btn-editar", function () {
+
+    if ($(this).closest("tr").hasClass("child")) {
+        filaSeleccionada = $(this).closest("tr").prev();
+    } else {
+        filaSeleccionada = $(this).closest("tr");
+    }
+    const data = tablaData.row(filaSeleccionada).data();
+
+    mostrarModal(data);
+
+})
+
+$("#tbdata tbody").on("click", ".btn-eliminar", function () {
+
+    let fila;
+    if ($(this).closest("tr").hasClass("child")) {
+        fila = $(this).closest("tr").prev();
+    } else {
+        fila = $(this).closest("tr");
+    }
+
+    const data = tablaData.row(fila).data();
+
+    swal({
+        title: "¿Está seguro?",
+        text: `Eliminar la categoria "${data.descripcion}"`,
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonClass: "btn-danger",
+        confirmButtonText: "Si, eliminar",
+        cancelButtonText: "No, cancelar",
+        closeOnConfirm: false,
+        closeOnCancel: true
+    },
+        function (respuesta) {
+
+            if (respuesta) {
+
+                $(".showSweetAlert").LoadingOverlay("show");
+
+                fetch(`/Categoria/Eliminar?IdCategoria=${data.idCategoria}`, {
+                    method: "DELETE"
+                })
+                    .then(response => {
+                        $(".showSweetAlert").LoadingOverlay("hide");
+                        return response.ok ? response.json() : Promise.reject(response);
+                    })
+                    .then(responseJson => {
+
+                        if (responseJson.estado) {
+
+                            tablaData.row(fila).remove().draw()
+
+                            swal("Listo!", "La categoria fue eliminada", "success")
+                        } else {
+                            swal("Lo sentimos", responseJson.mensaje, "error")
+                        }
+                    })
+
+
+            }
+        }
+    )
+
+
+})
