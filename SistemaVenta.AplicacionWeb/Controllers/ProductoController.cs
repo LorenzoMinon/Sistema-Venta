c3d9573884342390d6be59c258420a952e78d5ac
Lorenzo Miñón <lorenzominon01@gmail.com>
2024-02-07 09:11:38 -0300

    Implementación de solicitudes http para productos

--- SistemaVenta.AplicacionWeb/Controllers/ProductoController.cs
+++ SistemaVenta.AplicacionWeb/Controllers/ProductoController.cs
@@ -1,12 +1,127 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using AutoMapper;
+using Newtonsoft.Json;
+using SistemaVenta.AplicacionWeb.Models.ViewModels;
+using SistemaVenta.AplicacionWeb.Utilidades.Response;
+using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.Entity;
+
 namespace SistemaVenta.AplicacionWeb.Controllers
 {
     public class ProductoController : Controller
     {
+        private readonly IMapper _mapper;
+        private readonly IProductoService _productoServicio;
+
+        public ProductoController(IMapper mapper, IProductoService productoServicio, ICategoriaService categoriaServicio)
+        {
+            _mapper = mapper;
+            _productoServicio = productoServicio;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Lista()
+        {
+            List<VMProducto> vmProductoLista = _mapper.Map<List<VMProducto>>(await _productoServicio.Lista());
+
+            return StatusCode(StatusCodes.Status200OK, new { data = vmProductoLista });
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear([FromForm] IFormFile imagen, [FromForm] string modelo)
+        { //Que reciba del formulario l imagen y el miodelo en string.
+
+            GenericResponse<VMProducto> gResponse = new GenericResponse<VMProducto>();
+            try
+            {
+                VMProducto vmProducto = JsonConvert.DeserializeObject<VMProducto>(modelo);
+
+                string nombreImagen = "";
+                Stream imagenStream = null;
+                
+                if (imagen != null)
+                {
+                    string nombre_en_codigo = Guid.NewGuid().ToString("N");
+                    string extension = Path.GetExtension(imagen.FileName);
+                    nombreImagen = string.Concat(nombre_en_codigo, extension);
+                    imagenStream = imagen.OpenReadStream();
+                }
+                Producto producto_creado = await _productoServicio.Crear(_mapper.Map<Producto>(vmProducto), imagenStream, nombreImagen);
+
+
+                vmProducto = _mapper.Map<VMProducto>(producto_creado);
+
+                gResponse.Estado = true;
+                gResponse.Objeto = vmProducto;
+
+            }
+            catch(Exception ex)
+            {
+                gResponse.Estado = false;
+                gResponse.Mensaje = ex.Message;
+            }
+
+            List<VMProducto> vmProductoLista = _mapper.Map<List<VMProducto>>(await _productoServicio.Lista());
+            return StatusCode(StatusCodes.Status200OK, new { data = vmProductoLista });
+
+        }
+        [HttpPut]
+        public async Task<IActionResult> Editar([FromForm] IFormFile imagen, [FromForm] string modelo)
+        { 
+
+            GenericResponse<VMProducto> gResponse = new GenericResponse<VMProducto>();
+            try
+            {
+                VMProducto vmProducto = JsonConvert.DeserializeObject<VMProducto>(modelo);
+
+                Stream imagenStream = null;
+
+                if (imagen != null)
+                {
+                    imagenStream = imagen.OpenReadStream();
+                }
+                Producto producto_editado = await _productoServicio.Editar(_mapper.Map<Producto>(vmProducto), imagenStream);
+
+
+                vmProducto = _mapper.Map<VMProducto>(producto_editado);
+
+                gResponse.Estado = true;
+                gResponse.Objeto = vmProducto;
+
+            }
+            catch (Exception ex)
+            {
+                gResponse.Estado = false;
+                gResponse.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, gResponse);
+
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Eliminar(int idProducto)
+        {
+            GenericResponse<string> gResponse = new GenericResponse<string>();
+
+            try
+            {
+                gResponse.Estado = await _productoServicio.Eliminar(idProducto);
+
+            }
+            catch (Exception ex)
+            {
+                gResponse.Estado = false;
+                gResponse.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, gResponse);
+
+        }
+
+
     }
 }
