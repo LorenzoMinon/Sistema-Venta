e63a43b72a2b51620eab3a4f39d99b8c7145f38a
Lorenzo Miñón <lorenzominon01@gmail.com>
2024-02-14 09:49:29 -0300

    Get HTTP y modulo del reporte de ventas

--- SistemaVenta.AplicacionWeb/Controllers/ReporteController.cs
+++ SistemaVenta.AplicacionWeb/Controllers/ReporteController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using AutoMapper;
+using SistemaVenta.AplicacionWeb.Models.ViewModels;
+using SistemaVenta.BLL.Interfaces;
+
 namespace SistemaVenta.AplicacionWeb.Controllers
 {
     public class ReporteController : Controller
     {
+        private readonly IMapper _mapper;
+        private readonly IVentaService _ventaServicio;
+
+        public ReporteController(IMapper mapper, IVentaService ventaServicio)
+        {
+            _mapper = mapper;
+            _ventaServicio = ventaServicio;
+
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> ReporteVenta(string fechaInicio, string fechaFin)
+        {
+            List<VMReporteVenta> vmLista = _mapper.Map<List<VMReporteVenta>>(await _ventaServicio.Reporte(fechaInicio, fechaFin));
+
+            return StatusCode(StatusCodes.Status200OK, new { data = vmLista });
         }
     }
 }
---
+++ SistemaVenta.AplicacionWeb/wwwroot/js/vistas/Reporte_index.js
@@ -0,0 +1,61 @@
+﻿let tablaData;
+
+$(document).ready(function () {
+
+    $.datepicker.setDefaults($.datepicker.regional["es"])
+
+    $("#txtFechaInicio").datepicker({ dateFormat: "dd/mm/yy" })
+    $("#txtFechaFin").datepicker({ dateFormat: "dd/mm/yy" })
+
+    tablaData = $('#tbdata').DataTable({
+        responsive: true,
+        "ajax": {
+            "url": '/Reporte/ReporteVenta?fechaInicio=01/01/1991&fechaFin=14/02/2024',
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "fechaRegistro" },
+            { "data": "numeroVenta" },
+            { "data": "tipoDocumento" },
+            { "data": "documentoCliente" },
+            { "data": "nombreCliente" },
+            { "data": "subTotalVenta" },
+            { "data": "impuestoTotalVenta" },
+            { "data": "totalVenta" },
+            { "data": "producto" },
+            { "data": "cantidad" },
+            { "data": "precio" },
+            { "data": "total" },
+
+        ],
+        order: [[0, "desc"]], //Desde idCategoria
+        dom: "Bfrtip",
+        buttons: [
+            {
+                text: 'Exportar Excel',
+                extend: 'excelHtml5',
+                title: '',
+                filename: 'Reporte Ventas '
+            }, 'pageLength'
+        ],
+        language: { //para cambiar el idioma del datatable
+            url: "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json"
+        },
+    });
+
+})
+$("#btnBuscar").click(function () {
+
+    if ($("#txtFechaInicio").val().trim() == "" || $("#txtFechaFin").val().trim() == "") {
+        toastr.warning("", "Debe ingresar fecha inicio y fin")
+        return;
+    }
+
+    let fechaInicio = $("#txtFechaInicio").val().trim();
+    let fechaFin = $("#txtFechaFin").val().trim();
+
+    let nueva_url = `/Reporte/ReporteVenta?fechaInicio=${fechaInicio}&fechaFin=${fechaFin}`;
+
+    tablaData.ajax.url(nueva_url).load();
+})
