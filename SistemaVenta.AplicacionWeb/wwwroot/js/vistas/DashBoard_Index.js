fdefead693bbb3510681747176511f6677725b55
Lorenzo Miñón <lorenzominon01@gmail.com>
2024-02-16 11:54:00 -0300

    modulo dashboard

--- SistemaVenta.AplicacionWeb/wwwroot/js/vistas/DashBoard_Index.js
+++ SistemaVenta.AplicacionWeb/wwwroot/js/vistas/DashBoard_Index.js
@@ -1,1 +1,118 @@
-﻿
+﻿$(document).ready(function () {
+
+    $("div.container-fluid").LoadingOverlay("show");
+
+    fetch("/DashBoard/ObtenerResumen")
+        .then(response => {
+            $("div.container-fluid").LoadingOverlay("hide");
+            return response.ok ? response.json() : Promise.reject(response);
+        })
+        .then(responseJson => {
+
+            if (responseJson.estado) {
+                //mostrar datos para las tarjetas
+
+                let d = responseJson.objeto;
+
+                $("#totalVenta").text(d.totalVenta);
+                $("#totalIngresos").text(d.totalIngresos);
+                $("#totalProductos").text(d.totalProductos);
+                $("#totalCategorias").text(d.totalCategorias);
+
+                //obtener texts y valores para grafico de barras
+
+                let barchart_labels;
+                let barchart_data;
+
+                if (d.ventasUltimaSemana.length > 0) {
+                    barchart_labels = d.ventasUltimaSemana.map((item) => { return item.fecha; });
+                    barchart_data = d.ventasUltimaSemana.map((item) => { return item.total; });
+                } else {
+                    barchart_labels = ["Sin resultados"];
+                    barchart_data = [0];
+                }
+
+                // grafico de torta
+
+                let piechar_labels;
+                let piechar_data;
+
+                if (d.productosTopUltimaSemana.length > 0) {
+                    piechar_labels = d.productosTopUltimaSemana.map((item) => { return item.producto })
+                    piechar_data = d.productosTopUltimaSemana.map((item) => { return item.cantidad })
+                } else {
+                    piechar_labels = ["Sin resultados"]
+                    piechar_data = [0]
+                }
+
+                // Bar Chart 
+                let controlVenta = document.getElementById("chartVentas");
+                let myBarChart = new Chart(controlVenta, {
+                    type: 'bar',
+                    data: {
+                        labels: barchart_labels,
+                        datasets: [{
+                            label: "Cantidad",
+                            backgroundColor: "#4e73df",
+                            hoverBackgroundColor: "#2e59d9",
+                            borderColor: "#4e73df",
+                            data: barchart_data,
+                        }],
+                    },
+                    options: {
+                        maintainAspectRatio: false,
+                        legend: {
+                            display: false
+                        },
+                        scales: {
+                            xAxes: [{
+                                gridLines: {
+                                    display: false,
+                                    drawBorder: false
+                                },
+                                maxBarThickness: 50,
+                            }],
+                            yAxes: [{
+                                ticks: {
+                                    min: 0,
+                                    maxTicksLimit: 5
+                                }
+                            }],
+                        },
+                    }
+                });
+                // Pie Chart 
+                let controlProducto = document.getElementById("chartProductos");
+                let myPieChart = new Chart(controlProducto, {
+                    type: 'doughnut',
+                    data: {
+                        labels: piechar_labels,
+                        datasets: [{
+                            data: piechar_data,
+                            backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc', "#FF785B"],
+                            hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf', "#FF5733"],
+                            hoverBorderColor: "rgba(234, 236, 244, 1)",
+                        }],
+                    },
+                    options: {
+                        maintainAspectRatio: false,
+                        tooltips: {
+                            backgroundColor: "rgb(255,255,255)",
+                            bodyFontColor: "#858796",
+                            borderColor: '#dddfeb',
+                            borderWidth: 1,
+                            xPadding: 15,
+                            yPadding: 15,
+                            displayColors: false,
+                            caretPadding: 10,
+                        },
+                        legend: {
+                            display: true
+                        },
+                        cutoutPercentage: 80,
+                    },
+                });
+
+            }
+        });
+});
--- SistemaVenta.BLL/Implementacion/DashBoardService.cs
+++ SistemaVenta.BLL/Implementacion/DashBoardService.cs
@@ -14,34 +14,129 @@ namespace SistemaVenta.BLL.Implementacion
 {
     public class DashBoardService : IDashBoardService
     {
-        public Task<Dictionary<string, int>> ProductosTopUltimaSemana()
+        private readonly IVentaRepository _repositorioVenta;
+        private readonly IGenericRepository<DetalleVenta> _repositorioDetalleVenta;
+        private readonly IGenericRepository<Categoria> _repositorioCategoria;
+        private readonly IGenericRepository<Producto> _repositorioProducto;
+        private DateTime FechaInicio = DateTime.Now;
+
+        public DashBoardService(
+            IVentaRepository repositorioVenta,
+            IGenericRepository<DetalleVenta> repositorioDetalleVenta,
+            IGenericRepository<Categoria> repositorioCategoria,
+            IGenericRepository<Producto> repositorioProducto
+            )
         {
-             
+            _repositorioVenta = repositorioVenta;
+            _repositorioDetalleVenta = repositorioDetalleVenta;
+            _repositorioCategoria = repositorioCategoria;
+            _repositorioProducto = repositorioProducto;
+
+            FechaInicio = FechaInicio.AddDays(-7);  //Porque refiere a la ultima semana
+            
+        }
+        public async Task<int> TotalVentasUltimaSemana()
+        {
+            try
+            {
+                IQueryable<Venta> query = await _repositorioVenta.Consultar(v => v.FechaRegistro.Value.Date >= FechaInicio.Date);
+                int total = query.Count();
+                return total;
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+        public async Task<string> TotalIngresosUltimaSemana()
+        {
+            try
+            {
+                IQueryable<Venta> query = await _repositorioVenta.Consultar(v =>v.FechaRegistro.Value.Date >= FechaInicio.Date);
+
+                decimal resultado = query
+                    .Select(v => v.Total)
+                    .Sum(v => v.Value);
+
+                return Convert.ToString(resultado, new CultureInfo("es-AR"));
+
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+        public async Task<int> TotalProductos()
+        {
+            try
+            {
+                IQueryable<Producto> query = await _repositorioProducto.Consultar();
+                int total = query.Count();
+                return total;
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+        public async Task<int> TotalCategorias()
+        {
+            try
+            {
+                IQueryable<Categoria> query = await _repositorioCategoria.Consultar();
+                int total = query.Count();
+                return total;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public async Task<Dictionary<string, int>> VentasUltimaSemana()
+        {
+            try
+            {
+                IQueryable<Venta> query = await _repositorioVenta
+                    .Consultar(v => v.FechaRegistro.Value.Date >= FechaInicio.Date);
+
+                Dictionary<string, int> resultado = query
+                    .GroupBy(v => v.FechaRegistro.Value.Date).OrderByDescending(g => g.Key) //Agrupamos reporte de vta
+                    .Select(dv => new {fecha = dv.Key.ToString("dd/MM/yyyy"), total = dv.Count()}) //Creamos objeto con fecha y propiedad
+                    .ToDictionary(keySelector: r => r.fecha, elementSelector: r => r.total); // Creamos diccionario con propiedad de fecha y total
+
+                return resultado;
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public async Task<Dictionary<string, int>> ProductosTopUltimaSemana()
+        {
+            try
+            {
+                IQueryable<DetalleVenta> query = await _repositorioDetalleVenta.Consultar();
+
+                Dictionary<string, int> resultado = query
+                    .Include(v => v.IdVentaNavigation)
+                    .Where(dv => dv.IdVentaNavigation.FechaRegistro.Value.Date >= FechaInicio.Date)
+                    .GroupBy(dv => dv.DescripcionProducto).OrderByDescending(g => g.Count()) // Del mas vendido al menos vendido
+                    .Select(dv => new { producto = dv.Key, total = dv.Count() }).Take(4) 
+                    .ToDictionary(keySelector: r => r.producto, elementSelector: r => r.total); 
+
+                return resultado;
+
+            }
+            catch
+            {
+                throw;
+            }
         }
 
-        public Task<int> TotalCategorias()
-        {
 
-        }
-
-        public Task<string> TotalIngresosUltimaSemana()
-        {
-
-        }
-
-        public Task<int> TotalProductos()
-        {
-
-        }
-
-        public Task<int> TotalVentasUltimaSemana()
-        {
-
-        }
-
-        public Task<Dictionary<string, int>> VentasUltimaSemana()
-        {
-
-        }
     }
 }
