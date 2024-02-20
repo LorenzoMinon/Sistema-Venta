12c1fef2db04aaf1dcec3b9b4c7a71041bcb32cc
Lorenzo Miñón <lorenzominon01@gmail.com>
2024-02-20 18:15:41 -0300

    Implementación solicitudes http y modulo de perfil de usuario

--- SistemaVenta.AplicacionWeb/Controllers/HomeController.cs
+++ SistemaVenta.AplicacionWeb/Controllers/HomeController.cs
@@ -6,15 +6,24 @@ using System.Diagnostics;
 using System.Net;
 using System.Security.Claims;
 
+using AutoMapper;
+using SistemaVenta.AplicacionWeb.Models.ViewModels;
+using SistemaVenta.AplicacionWeb.Utilidades.Response;
+using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.Entity;
+
 namespace SistemaVenta.AplicacionWeb.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IUsuarioService _usuarioServicio;
+        private readonly IMapper _mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUsuarioService usuarioServicio, IMapper mapper)
         {
-            _logger = logger;
+            _usuarioServicio = usuarioServicio;
+            _mapper = mapper;
+            
         }
 
         public IActionResult Index()
@@ -29,6 +38,86 @@ namespace SistemaVenta.AplicacionWeb.Controllers
         public IActionResult Perfil()
         {
             return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> ObtenerUsuario()
+        {
+            GenericResponse<VMUsuario> response = new GenericResponse<VMUsuario>();
+            try
+            {
+                ClaimsPrincipal claimUser = HttpContext.User;
+
+                string idUsuario = claimUser.Claims
+                    .Where(c => c.Type == ClaimTypes.NameIdentifier) //Propiedad declarada en acceso controller
+                    .Select(c => c.Value).SingleOrDefault();
+
+                VMUsuario usuario = _mapper.Map<VMUsuario>(await _usuarioServicio.ObtenerPorId(int.Parse(idUsuario)));
+
+                response.Estado = true;
+                response.Objeto = usuario;
+            }
+            catch (Exception ex)
+            {
+                response.Estado = false;
+                response.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> GuardarPerfil([FromBody] VMUsuario modelo)
+        {
+            GenericResponse<VMUsuario> response = new GenericResponse<VMUsuario>();
+            try
+            {
+                ClaimsPrincipal claimUser = HttpContext.User;
+
+                string idUsuario = claimUser.Claims
+                    .Where(c => c.Type == ClaimTypes.NameIdentifier) //Propiedad declarada en acceso controller
+                    .Select(c => c.Value).SingleOrDefault();
+
+                Usuario entidad = _mapper.Map<Usuario>(modelo);
+
+                entidad.IdUsuario = int.Parse(idUsuario);
+
+                bool resultado = await _usuarioServicio.GuardarPerfil(entidad);
+
+                response.Estado = resultado;
+                
+            }
+            catch (Exception ex)
+            {
+                response.Estado = false;
+                response.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CambiarClave([FromBody] VMCambiarClave modelo)
+        {
+            GenericResponse<bool> response = new GenericResponse<bool>();
+            try
+            {
+                ClaimsPrincipal claimUser = HttpContext.User;
+
+                string idUsuario = claimUser.Claims
+                    .Where(c => c.Type == ClaimTypes.NameIdentifier) //Propiedad declarada en acceso controller
+                    .Select(c => c.Value).SingleOrDefault();
+
+                bool resultado = await _usuarioServicio.CambiarClave(
+                    int.Parse(idUsuario),
+                    modelo.ClaveActual,
+                    modelo.ClaveNueva
+                    );
+
+                response.Estado = resultado;
+            }
+            catch (Exception ex)
+            {
+                response.Estado = false;
+                response.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, response);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ SistemaVenta.AplicacionWeb/wwwroot/js/vistas/Home_Perfil.js
@@ -0,0 +1,130 @@
+﻿
+$(document).ready(function () {
+    $(".container-fluid").LoadingOverlay("show");
+
+    fetch("/Home/ObtenerUsuario")
+        .then(response => {
+            $(".container-fluid").LoadingOverlay("hide");
+            return response.ok ? response.json() : Promise.reject(response);
+        })
+        .then(responseJson => {
+
+            if (responseJson.estado) {
+                const d = responseJson.objeto
+
+                $("#imgFoto").attr("src", d.urlFoto)
+                $("#txtNombre").val(d.nombre)
+                $("#txtCorreo").val(d.correo)
+                $("#txTelefono").val(d.telefono)
+                $("#txtRol").val(d.nombreRol)
+
+            } else {
+                swal("Lo sentimos", responseJson.mensaje, "error")
+            }
+        })
+})
+
+$("#btnGuardarCambios").click(function () {
+
+    if ($("#txtCorreo").val().trim() == "") {
+        toast.warning("", "Debe completar el campo : correo")
+        $("#txtCorreo").focus() //el cursor se mueve directamente al campo con txtdesc
+        return;
+    }
+
+    if ($("#txTelefono").val().trim() == "") {
+        toast.warning("", "Debe completar el campo : teléfono ")
+        $("#txTelefono").focus() //el cursor se mueve directamente al campo con txtdesc
+        return;
+    }
+
+    swal({
+        title: "¿Desea guardar los cambios?",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonClass: "btn-primary",
+        confirmButtonText: "Si",
+        cancelButtonText: "No",
+        closeOnConfirm: false,
+        closeOnCancel: true
+    },
+        function (respuesta) {
+
+            if (respuesta) { // Si se presiona si esto devuelve TRUE
+
+                $(".showSweetAlert").LoadingOverlay("show");
+
+                let modelo = {
+                    correo: $("#txtCorreo").val().trim(),
+                    telefono: $("#txTelefono").val().trim()
+                }
+
+                fetch("Home/GuardarPerfil", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json; charset=utf-8" },
+                    body: JSON.stringify(modelo)
+                })
+                    .then(response => {
+                        $(".showSweetAlert").LoadingOverlay("hide");
+                        return response.ok ? response.json() : Promise.reject(response);
+                    })
+                    .then(responseJson => {
+
+                        if (responseJson.estado) {
+                            swal("Listo!", "Los cambios fueron realizados", "success")
+                        } else {
+                            swal("Lo sentimos", responseJson.mensaje, "error")
+                        }
+                    })
+
+
+            }
+        }
+    )
+
+})
+
+$("#btnCambiarClave").click(function () {
+
+    const inputs = $("input.input-validar").serializeArray();
+    const inputs_sin_valor = inputs.filter((item) => item.value.trim() == "")
+
+    if (inputs_sin_valor.length > 0) {
+        const mensaje = `Debe completar el campo: "${inputs_sin_valor[0].name}"`;
+        toastr.warning("", mensaje) // sale del wwwroot, vendor y el toastr.
+        $(`input[name="${inputs_sin_valor[0].name}"]`).focus()
+        return;
+    }
+    if ($("#txtClaveNueva").val().trim() != $("#txtConfirmarClave").val().trim()) {
+        toastr.warning("", "Las contraseñas no coinciden")
+        return;
+    }
+    
+    let modelo = {
+        claveActual: $("#txtClaveActual").val().trim(),
+        claveNueva: $("#txtClaveNueva").val().trim()
+    }
+
+    fetch("/Home/CambiarClave", {
+        method: "POST",
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        body: JSON.stringify(modelo)
+    })
+        .then(response => {
+            $(".showSweetAlert").LoadingOverlay("hide");
+            return response.ok ? response.json() : Promise.reject(response);
+        })
+        .then(responseJson => {
+
+            if (responseJson.estado) {
+                swal("Listo!", "Su contraseña ha sido cambiada con éxito ", "success")
+                // Limpiar las cajas de texto
+                $("#txtClaveActual").val("");
+                $("#txtClaveNueva").val("");
+                $("#txtConfirmarClave").val("");
+            } else {
+                swal("Lo sentimos", responseJson.mensaje, "error")
+            }
+        })
+
+})
